9ade55436e8142340c6b3c1d76691a8081436474
blocksurance-dao <alexei@blocksurance.io>
2022-05-31 23:03:52 +0100

    mock

--- contracts/mocks/MockContract.sol
+++ contracts/mocks/MockContract.sol
@@ -4,19 +4,17 @@ pragma solidity ^0.8.0;
 contract MockContract {
 	address public lastReceiver;
 
-	function _transfer(address payable _reciver, uint256 amount)
+	function _transfer(address payable receiver, uint256 amount)
 		public
 		payable
 	{
-		_reciver.transfer(amount);
-
-		lastReceiver = _reciver;
+		lastReceiver = receiver;
+		receiver.transfer(amount);
 	}
 
-	function _send(address payable _reciver, uint256 amount) public payable {
-		bool sent = _reciver.send(amount);
+	function _send(address payable receiver, uint256 amount) public payable {
+		lastReceiver = receiver;
+		bool sent = receiver.send(amount);
 		require(sent, "Failed to send Ether");
-
-		lastReceiver = _reciver;
 	}
 }
